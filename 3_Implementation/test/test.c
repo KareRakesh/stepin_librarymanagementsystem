47b052cdfa5f9e23474bd878e52cde6e4ff805fe
KareRakesh <89700388+KareRakesh@users.noreply.github.com>
2021-09-07 22:25:36 +0530

    Add files via upload

---
+++ 3_Implementation/test/test.c
@@ -0,0 +1,43 @@
+/**
+ * @file test_student_management.c
+ * @author Kare Rakesh 99006138
+ * @brief 
+ * @version 0.1
+ * 
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "unity.h"
+#include "student.h"
+
+/* Modify these two lines according to the project */
+#define PROJECT_NAME    "Student"
+
+/* Prototypes for all the test functions */
+void test_stud(void);
+
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+/* Start of the application test */
+int main()
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_stud);
+
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
+
+/* Write all the test functions */ 
+void test_stud(void) {
+  TEST_ASSERT_EQUAL(SUCCESS, Add());
+
+}
